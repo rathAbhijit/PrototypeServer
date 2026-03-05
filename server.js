72165c0cfe94a17d608894b4de8367722fb4c6ed
rathAbhijit <rathabhijit40@gmail.com>
2026-03-05 12:04:05 +0530

    initial commit

---
+++ server.js
@@ -0,0 +1,16 @@
+import http from "http";
+import { WebSocketServer } from "ws";
+import { setupWSConnection } from "y-websocket/bin/utils.js";
+
+const port = 1234;
+
+const server = http.createServer();
+const wss = new WebSocketServer({ server });
+
+wss.on("connection", (conn, req) => {
+  setupWSConnection(conn, req);
+});
+
+server.listen(port, () => {
+  console.log(`Yjs server running at ws://localhost:${port}`);
+});
