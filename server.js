669158d446a45483b8c96b65f97881df37cefa1f
rathAbhijit <rathabhijit40@gmail.com>
2026-03-06 07:13:29 +0530

    render ready6

--- server.js
+++ server.js
@@ -1,20 +1,16 @@
 import http from "http";
-import WebSocket, { WebSocketServer } from "ws";
-import * as Y from "yjs";
-import { setPersistence, setupWSConnection } from "y-websocket";
+import { WebSocketServer } from "ws";
+import { setupWSConnection } from "y-websocket/bin/utils.js";
 
 const port = process.env.PORT || 1234;
 
 const server = http.createServer();
-
-const wss = new WebSocketServer({
-  server
-});
+const wss = new WebSocketServer({ server });
 
 wss.on("connection", (conn, req) => {
   setupWSConnection(conn, req);
 });
 
-server.listen(port, "0.0.0.0", () => {
-  console.log(`Yjs websocket server running on port ${port}`);
+server.listen(port, () => {
+  console.log(`Yjs server running at ws://localhost:${port}`);
 });
